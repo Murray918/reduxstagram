df5f9e5b67819c03249c8e4a0e7d32b41b48f1f7
Andrew Murray <darbyguitars@gmail.com>
2017-12-22 15:29:51 -0600

    more components filles

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Main = React.createClass({
+  render(){
+    return(
+      <div className = 'signle-photo'>
+        i am the single
+      </div>
+    )
+  }
+})
+
+export default Main
